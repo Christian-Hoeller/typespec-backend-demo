ecdb8673fe8ae7cf7fa7f90a6487f423a1168fae
Sev <sevm@pm.me>
2024-03-06 12:55:14 +0100

    feat: added types package

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import app from "./app";
+import seas from "@christian-hoeller/zod-sst-demo";
 
 const PORT = 4040;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+
+  seas();
 });
